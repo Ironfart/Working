881bfa228e7df2295750ecb1571c27f6cf06e7c3
bestjonav2 <jona622@hotmail.com>
2017-08-05 19:38:13 -0600

    sm

---
+++ app/Http/Controllers/AsociadosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AsociadosController extends Controller{
+    public function index(){
+      return view("asociados");
+    }
+
+    public function onClick(){
+
+    }
+}
